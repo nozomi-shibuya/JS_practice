56b670d4fe9e5871d2ca80e69337a9266cbfe29e
Kinoko <4kinoko.non@gmail.com>
2023-11-05 12:35:26 +0900

    JS_課題０６

---
+++ assignment06.js
@@ -0,0 +1,14 @@
+// 配列の定義
+let fruit= ['apricot', 'strawberry'];
+
+// 配列の末尾に追加
+fruit.push('grape');
+
+// 配列の先頭に追加
+fruit.unshift('pear');
+
+// 末尾のデータを'lime'に書き換える
+fruit[3] = 'lime'
+
+// 結果の出力
+console.log(fruit);
