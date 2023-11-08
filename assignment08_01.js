b857d9f02a5ca838a318c9470c9141ddda92e9f7
Kinoko <4kinoko.non@gmail.com>
2023-11-08 18:46:21 +0900

    JS_課題０８

---
+++ assignment08_01.js
@@ -0,0 +1,7 @@
+//関数名がある場合
+function getTriangleArea(base,height){
+    return base * height / 2;
+}
+let area = getTriangleArea(10,4);
+console.log(area);
+
---
+++ assignment08_02.js
@@ -0,0 +1,6 @@
+//関数名がない場合
+var traigangle = function(base,height){
+    return base*height/2;
+}(10,4);
+console.log(traigangle);
+
