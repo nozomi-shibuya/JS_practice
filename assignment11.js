79675ba9ffb3708e8208cea019c1e0cb38a4a506
Kinoko <4kinoko.non@gmail.com>
2023-11-19 17:57:43 +0900

    課題１１

---
+++ assignment11.js
@@ -0,0 +1,12 @@
+let e1 = document.getElementById('p1');
+let e2 = document.getElementById('p2');
+let e3 = document.getElementById('p3');
+
+console.log(e1.tagName);
+e1.tagName = ('input');
+
+console.log('p2');
+e2.innerText = '<p>テキスト文字列</p>';
+
+console.log(e3.id);
+e3.id = 'p5';
