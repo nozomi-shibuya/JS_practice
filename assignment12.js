dbcafb2b3444cfe898a3333fb2d17edeb3ea2a0c
Kinoko <4kinoko.non@gmail.com>
2023-11-30 15:27:49 +0900

    課題１２再提出

--- assignment12.js
+++ assignment12.js
@@ -1,9 +1,16 @@
 'use strict';
 
-{   
-    document.querySelector('input').addEventListener('input', () => {
-    const pElement = document.querySelector('span');
+{
+  
+  document.querySelector('input').addEventListener('input', () => {
+    const pElement = document.querySelector('p');
     const inputElement = document.querySelector('input');
     pElement.textContent = inputElement.value;
   });
+
+  document.querySelector('input').addEventListener('blur', () => {
+    document.querySelector('p').textContent = '';
+  });
+
+  document.querySelector('input').focus();
 }
